b87e8904fe35ebe3fce5e52968319599c724d2e4
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2025-03-22 21:28:37 +0800

    feat: add shutdown to server

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 	"net/rpc"
 	"net/rpc/jsonrpc"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 type Arith struct{}
@@ -20,25 +25,48 @@ func (t *Arith) Add(args *Args, reply *int) error {
 
 func main() {
 	arith := new(Arith)
-	err := rpc.Register(arith)
-	if err != nil {
+	if err := rpc.Register(arith); err != nil {
 		log.Fatal("Failed to register:", err)
 	}
 
-	// 監聽 TCP 端口
 	listener, err := net.Listen("tcp", ":1234")
 	if err != nil {
 		log.Fatal("Failed to listen:", err)
 	}
 	defer listener.Close()
 
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := &sync.WaitGroup{}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		log.Println("Shutting down server...")
+		cancel()
+		listener.Close()
+	}()
+
 	log.Println("JSON-RPC Server is running on port 1234")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Println("Failed to accept:", err)
+			select {
+			case <-ctx.Done():
+				wg.Wait()
+				log.Println("Server stopped.")
+				return
+			default:
+				log.Println("Failed to accept:", err)
+			}
 			continue
 		}
-		go jsonrpc.ServeConn(conn)
+
+		wg.Add(1)
+		go func(conn net.Conn) {
+			defer wg.Done()
+			jsonrpc.ServeConn(conn)
+		}(conn)
 	}
 }
